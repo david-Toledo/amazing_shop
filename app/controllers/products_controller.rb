e34b7a384d7aff7ee5a5446544e027b1c3d90425
david <davidtoledo87@hotmail.com>
2020-12-07 19:33:07 +1100

    arthur

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,32 @@
+class ProductsController < ApplicationController
+
+#create
+  def new
+  end
+
+  def create
+  end
+
+#read
+
+  def index
+    @products=Product.all
+  end
+
+  def show
+  end
+
+#update
+
+  def edit
+  end
+
+  def update
+  end
+
+
+#destroy
+
+  def destroy
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,18 +18,46 @@ user2 = User.create! email:'luke@ga.co', password:'pass', username:'luke_sei', i
 
 user3 = User.create! email:'zara@ga.co', password:'pass', username:'zara_sei', image:"https://place-puppy.com/300x600"
 
+puts "created users: #{User.count}"
 
 
-# puts "creating products"
-#
-# Product.destroy_all
-#
-# product1 = Product.
-#
-#
-# t.string :title
-# t.string :brand
-# t.string :department
-# t.text :description
-# t.decimal :price, precision:10, scale:2
-# t.text :image
+puts "creating products....."
+
+Product.destroy_all
+
+product1 = Product.create! title:'tpg modem', brand:'tpg', department:"electronics", description: "fantastic modem for the householder", price:99.90, image:"https://place-puppy.com/300x600"
+
+product2 = Product.create! title:'samsung tv 55 inches', brand:'samsung', department:"electronics", description: "fantastic modem for the householder", price:999.90, image:"https://place-puppy.com/300x600"
+
+product3 = Product.create! title:'ethernet cable', brand:'link', department:"electronics", description: "fantastic modem for the householder", price:10.00, image:"https://place-puppy.com/300x600"
+
+puts "created products: #{Product.count}"
+
+puts "creating baskets ....."
+
+
+Basket.destroy_all
+
+basket1 = Basket.create! user_id:user1.id
+basket2 = Basket.create! user_id:user2.id
+basket3 = Basket.create! user_id:user3.id
+
+puts "created baskets: #{Basket.count}"
+
+
+
+puts "creating line_items....."
+
+LineItem.destroy_all
+
+line_item1 = LineItem.create! quantity:5, basket_id:basket1.id, product_id:product1.id
+line_item2 = LineItem.create! quantity:3, basket_id:basket2.id, product_id:product1.id
+line_item3 = LineItem.create! quantity:10, basket_id:basket3.id, product_id:product1.id
+line_item4 = LineItem.create! quantity:8, basket_id:basket1.id, product_id:product2.id
+line_item5 = LineItem.create! quantity:2, basket_id:basket2.id, product_id:product2.id
+line_item6 = LineItem.create! quantity:9, basket_id:basket3.id, product_id:product3.id
+line_item7 = LineItem.create! quantity:15, basket_id:basket1.id, product_id:product3.id
+
+
+puts "created line_items: #{LineItem.count}"
+puts LineItem.pluck :basket_id
