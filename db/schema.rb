e808b30532e7febe840f93d81c33031317ede036
david <davidtoledo87@hotmail.com>
2020-12-10 22:19:53 +1100

    admin_user_added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_000304) do
+ActiveRecord::Schema.define(version: 2020_12_10_103524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_000304) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
